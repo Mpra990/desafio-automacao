fa99404b644117afa70f451d08f851b63fd9b243
Mpra990 <mpra1210@gmail.com>
2026-05-05 10:41:52 -0300

    fix: adicionando imports e suporte ao chrome

--- web_testing/conftest.py
+++ web_testing/conftest.py
@@ -1,22 +1,20 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import platform
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def driver():
     options = Options()
-    # Lógica Híbrida: Brave no seu PC, Chrome no GitHub
     if platform.system() == "Windows":
-        options.binary_location = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+        # Remova a linha do binary_location para usar o Chrome padrão
+        pass 
     else:
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver.implicitly_wait(10)
     yield driver
     driver.quit()
